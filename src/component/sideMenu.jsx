a25c4c5e67281439723f60172b6dd29bb71dfe37
Aditya Aditya <c0912729@mylambton.ca>
2024-11-07 15:26:08 -0500

    userPage done

--- src/component/sideMenu.jsx
+++ src/component/sideMenu.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../assets/css/component/sideMenu.scss";
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { FaHome, FaUser, FaCog } from "react-icons/fa"; // Using react-icons for icons
+import { FaUserPlus } from "react-icons/fa6";
 
 const SideMenu = () => {
   const location = useLocation(); // Get current route using useLocation
@@ -44,6 +45,21 @@ const SideMenu = () => {
                   <span>Profile</span>
                 </Nav.Link>
               </Nav.Item>
+                  
+              <Nav.Item>
+                <Nav.Link 
+                  href="/Userpage"
+                  className={`text-dark ${ 
+                    activeKey === "/UserPage"?"bg-selected": ""
+                  }`}
+                  >
+                    <FaUserPlus className="me-2"/>
+                    <span>Add User</span>
+                  </Nav.Link>             
+              </Nav.Item>
+
+              
+
               <Nav.Item>
                 <Nav.Link
                   href="/settings"
--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,10 +1,12 @@
 // src/appRoutes.js
 import HomePage from "../views/HomePage";
 import ProfilePage from "../views/ProfilePage";
+import UserPage from "../views/UserPage";
 
 const routes = [
   { path: "/home", element: <HomePage /> },
   { path: "/profile", element: <ProfilePage /> },
+  {path:"/UserPage", element:<UserPage/>}
 ];
 
 export default routes;
